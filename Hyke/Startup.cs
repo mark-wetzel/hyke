8827616433d67f65ab555b84a333b1d0af9f673e
Mark Wetzel <me@markwetzel.com>
2016-05-21 20:34:19 -0600

    Initial

---
+++ Hyke/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hyke.Startup))]
+namespace Hyke
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
